b0f33b0fa04d2546ae3a0ca28506151c814e63a2
annisadiann <annisadian0412@gmail.com>
2024-03-08 11:28:33 +0700

    Deklarasi array

--- PrakAlgoritma_1/PrakAlgoritma_1.cpp
+++ PrakAlgoritma_1/PrakAlgoritma_1.cpp
@@ -4,6 +4,6 @@
 using namespace std;
 
 int a[20];			// Deklarasi array a dengan ukuran 20
-int n;				// Deklarasi variabel n untuk menyimpan banyaknya elemen pada array 
+int n;				// Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 
